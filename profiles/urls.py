cf02c9d77032ad01f37556a4ab4dffc53f7112ee
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2024-05-01 22:42:09 +1000

    Profile Update

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'profiles'
 
 urlpatterns = [
-    path("detail/<int:pk>", views.UserProfile.as_view(), name = 'profile_detail')
+    path("detail/<int:pk>", views.UserProfile.as_view(), name = 'profile_detail'),
+    path("update/<int:pk>", views.UpdateProfile.as_view(), name = 'profile_update')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Profile
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 class UserProfile(DetailView):
@@ -9,3 +10,10 @@ class UserProfile(DetailView):
     http_method_names = ["get"]
     context_object_name = "profile"
     pk_url_kwarg = 'pk'
+
+class UpdateProfile(UpdateView):
+    model = Profile
+    fields = ['first_name', 'last_name', 'mobile', 'address']
+    
+    def get_success_url(self):
+        return reverse_lazy("profiles:profile_detail", kwargs = {"pk":self.kwargs["pk"]})
